53f42c2ef595acaa6e744cbf2b8b02c54420e62f
jonathanjp0516 <jonathanjp0516@gmail.com>
2026-03-18 13:04:09 +0800

    week4 hw

---
+++ C112152327_week4_1.cpp
@@ -0,0 +1,23 @@
+//brute force GCD
+#include<stdio.h>
+
+int gcd_brute_force_func(const int a, const int b){
+    if(a == 0 || b == 0) return (a > b) ? a : b;//zero exception
+    int min = (a < b) ? a : b;//find the smaller one
+
+    for(int i = min; i > 0; i--){
+        if(a % i == 0 && b % i == 0) return i;
+    }
+    return 1;
+}
+
+int main(){
+    int a, b;
+    while(true){
+        printf("Enter two numbers: ");
+        scanf("%d %d", &a, &b);
+        printf("%d\n", gcd_brute_force_func(a, b));
+    }
+    
+    return 0;
+}
