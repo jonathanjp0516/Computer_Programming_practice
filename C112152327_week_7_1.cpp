3a6fa44cb23905e0705c31d5468c2e9a57fbf025
JY <jonathanjp0516@gmail.com>
2026-04-14 18:26:53 +0800

    new

--- C112152327_week_7_1.cpp
+++ C112152327_week_7_1.cpp
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include <stdbool.h>
 
+float fact(const int num){
+    if(num==1){
+        return 1;
+    }else{
+        return num * fact(num - 1);
+    }
+}
+
 int main(){
-    
+    int num = 0,sum = 0;
+    while(1){
+        scanf("%d",&num);
+        for(int i=0;i<=num;i++){
+
+        }
+    }
 }
---
+++ C112152327_week_7_2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include <stdbool.h>
+
+typedef enum{
+    
+}cord;
+
+float dist(const float x1,const float x2,const float y1,const float y2){
+    return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+
+float heron_f(const float a,const float b,const float c){
+}
+
+int main(){
+    int num = 0,sum = 0;
+    while(1){
+        scanf("%d",&num);
+        for(int i=0;i<=num;i++){
+
+        }
+    }
+}
